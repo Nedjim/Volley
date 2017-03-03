2eb5956e8edcdebecf304d8d174b7d3c0b9780e7
Nedjim <nedjim.danimon@gmail.com>
2017-03-03 18:00:06 +0100

    add link navigation

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 //Dependencies
 var express = require('express');
 var path = require('path');
+
 var home = require('./routes/home');
 var practice = require('./routes/practice');
 var frenchTeam = require('./routes/frenchTeam');
 var competitions = require('./routes/competitions');
 var contact = require('./routes/contact');
-//application express
+
 var app = express();
 
-//config : jade, dossiers view
 app.set('view engine','jade');
 app.set('views', path.join(__dirname, 'views'));
-//dossier static
+
 app.use(express.static('public'));
 
-//Lien vers home.js
 app.use('/', home);
+
 app.use('/pratiquer', practice);
 app.use('/equipesfr', frenchTeam);
 app.use('/competitions', competitions);
 app.use('/contact', contact);
 
-//Port d'écoute
 app.listen('3000');
 console.log('Port 3000');
